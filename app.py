9178fb009d62eeb8d44ab1ba16208fa7c8514113
anutriv <anoop.trivedi@gmail.com>
2025-05-18 15:20:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask_cors import CORS  # Import CORS
 from pydub import AudioSegment
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
+CORS(app, supports_credentials=True)
+
 
 UPLOAD_FOLDER = "uploads"
 PROCESSED_FOLDER = "processed"
